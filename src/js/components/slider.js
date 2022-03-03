c2143f9e72d0e3a4006afe37f0442e8b1dd4b772
Adam <66186445+AdamabuMedina@users.noreply.github.com>
2022-03-03 18:23:49 +0300

    trainers сверстано

--- src/js/components/slider.js
+++ src/js/components/slider.js
@@ -7,4 +7,12 @@ const swiper = new Swiper(el, {
   slidesPerView: 'auto',
   // slidesPerView: 4,
   spaceBetween: 40,
+  scrollbar: {
+    el: ".trainers__scroll",
+    draggable: true,
+  },
+  navigation: {
+    nextEl: ".trainers__slider-btn--next",
+    prevEl: ".trainers__slider-btn--prev",
+  },
 });
